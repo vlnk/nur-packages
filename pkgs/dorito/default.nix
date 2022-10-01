a57df5808d84a94a043a3f0765e5b72ba27e654b
vlnk <vlnk@protonmail.com>
2022-10-01 16:35:26 -0400

    🚧 add broken dorito package

---
+++ pkgs/dorito/default.nix
@@ -0,0 +1,23 @@
+{ lib, stdenv, fetchgit, ninja, cmake, pkg-config }:
+
+stdenv.mkDerivation rec {
+  pname = "dorito";
+  version = "0.5.0";
+
+  src = fetchgit rec {
+    url = "https://github.com/lesharris/dorito";
+    fetchSubmodules = true;
+    rev = "${version}";
+    sha256 = "wvVqvfMP4KTMpu1pAePzJIzUEQyx8GJjXqJxjVysWzc=";
+  };
+
+  nativeBuildInputs = [ cmake ninja pkg-config ];
+  cmakeFlags = [ "-G Ninja" ];
+
+  meta = with lib; {
+    description = "A Dev Environment for Chip8, SuperChip, and XO-Chip";
+    license = licenses.unlicense;
+    maintainers = with maintainers; [ vlnk ];
+    platforms = platforms.unix;
+  };
+}
